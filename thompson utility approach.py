9cdc6a54d897ae0df1599b69c0acb2b73823c966
Samuel Lee <samuel-lwl@outlook.com>
2019-03-14 18:12:38 +0800

    Create thompson utility approach.py

---
+++ thompson utility approach.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 12 16:59:36 2019
+
+@author: Samuel
+"""
+
+import pandas as pd
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+from math import sqrt
+from math import log
+
+x = pd.read_excel("C:/Uninotes/FYP/data/selected-sales data_children%27s book_every 99 cut 50.xlsx") # desktop
+#x = pd.read_excel("C:/Users/Samuel/Desktop/uninotes/FYP/selected-sales data_children%27s book_every 99 cut 50.xlsx") # laptop
+
+# removed brand_id, agio_cut_price and free_cut_price
+dataoriginalprice = x.iloc[:,[1,17]]
+datafullcut = x.iloc[:,[1,19]]
+
+# merge based on order id for cut_price
+datafullcut = datafullcut.groupby(['order_id']).sum().reset_index()
+
+# merge based on order id for price per order
+dataoriginalprice = dataoriginalprice.groupby(['order_id']).sum().reset_index()
+
+# merge both together, sort by order amount
+datamerge = pd.merge(dataoriginalprice,datafullcut)
+datamerge = datamerge.sort_values(by='goods_money')
+
+# remove outlier index 1139 where cut_goods_money = 0.35
+datamerge.drop(1139,inplace=True)
+
+
+# changes goods_money to reflect price of just 1 quantity
+    
+# remove outlier index 2066 where cut_goods_money = 0.35
+x.drop(2066,inplace=True)
+
+# sort by goods_id and reset index
+productprice=x.iloc[:,[5,17,13]]
+productprice.sort_values(by='goods_id', axis=0, ascending=True, inplace=True)
+productprice.reset_index(level=None, drop=True, inplace=True)
+
+# note: there are 3 products in 3 orders with 0 quantity ordered cos they were returned.
+# here, just obtaining price, qty ordered not important
+for i, row in productprice.iterrows():
+    if row["goods_amount"]==2:
+        productprice.at[i, "goods_money"] = productprice.at[i, "goods_money"]/2
+        
+#remove goods_amount 
+productprice=productprice.iloc[:,[0,1]]
+
+numvars = x.nunique()[5]
+# x.nunique() tells us there are 66 unique goods   
+# obtain price for these 66 goods     
+productprice.drop_duplicates(subset="goods_id", keep='first', inplace=True)
+productprice.reset_index(level=None, drop=True, inplace=True)
+
+productprice = productprice.iloc[:,1].values.reshape((numvars,1))
+
+# MVN parameters for true V
+vmean = productprice
+vcov = np.identity(numvars)
+
+num_data = 20
+v_prior = np.empty((num_data, numvars))
+for i in range(num_data):
+    v_prior[i] = np.random.multivariate_normal(vmean.flatten(), vcov, 1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
