f95b175e97efc6e09e223d4a5c653934f385c9f3
Samuel Lee <samuel-lwl@outlook.com>
2019-03-18 08:07:57 +0800

    Update thompson utility approach.py
    Implemented cvxopt, with error "Rank(A) < p or Rank([H(x); A; Df(x); G]) < n"

--- thompson utility approach.py
+++ thompson utility approach.py
@@ -67,7 +67,6 @@ productprice = productprice.iloc[:,1].values.reshape((numvars,1))
 
 
 
-
 # True distribution of V (MVN distribution)
 vmean = productprice
 vcov = np.identity(numvars)
@@ -126,6 +125,49 @@ for i in range(data_v.shape[0]):
     v_cov += np.matmul(temp1, temp2)
 # divide by number of data points since MLE estimate of cov matrix is divide by N not N-1
 v_cov /= data_v.shape[0]
+
+
+v_estimate = np.random.multivariate_normal(v_mean.flatten(), v_cov, 1)
+
+
+from cvxopt import solvers, matrix, spdiag, log, exp, div, mul
+
+
+
+A, b = matrix(1.0, (1, numvars+1)), matrix(1.0)
+V = matrix(v_estimate, (numvars,1))
+# Adding in v0 = 0
+V = matrix([0, V])
+V = V.T
+
+# maximize   V*x + x[1:].T*matrix(np.log(x[1:])) + (1 - x[0])*math.log(x[0])
+# subject to A*x = b
+
+def F(x=None, z=None):
+    # Set sample point x0 as 1/(number of variables)
+    if x is None: return 0, matrix(1/(numvars+1), (numvars+1,1))
+    
+    # products' demand cannot be negative
+    if min(x) < 0: return None
+    
+    # Objective function, negative sign since we want to maximise
+    f = -(V*x - x[1:].T*log(x[1:]) + (1-x[0])*log(x[0]))
+    
+    # Vector of first partial derivatives
+    grad = V.T + mul(x, log(x))  
+    grad[0] = (1-x[0])/x[0] - log(x[0])
+    if z is None: return f, grad.T
+    
+    # Hessian, why only z[0]?
+    H = - 1 - log(x)
+    H[0] = -(x[0]**-2)
+    H = spdiag(z[0] * H)
+    return f, grad.T, H
+sol = solvers.cp(F, A=A, b=b) 
+p = sol['x']
+
+
+
 
 
 
